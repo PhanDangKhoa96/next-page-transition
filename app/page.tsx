80365f28887d214e73870803748a608b89907105
Khoa Phan <pldkhoa96.work@gmail.com>
2025-01-12 23:03:33 +0700

    feat: update Home component layout to use flexbox for improved responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HeroHome from "./_components/HeroHome";
 
 export default function Home() {
     return (
-        <main className="mx-[2vw]">
+        <main className="mx-[2vw] flex-1 flex">
             <HeroHome />
         </main>
     );
